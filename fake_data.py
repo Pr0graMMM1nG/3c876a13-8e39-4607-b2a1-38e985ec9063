64191011d375a0ef8f076296ac215b79527e11d6
alg0r1txm <alg0r1txm@protonmail.com>
2025-05-01 10:02:40 +0000

    create fake data to save in database using Faker module
    fake data: user name, birth

---
+++ fake_data.py
@@ -0,0 +1,54 @@
+from enum import Enum
+
+from faker import Faker
+
+from models import Address, City, Street, User
+
+fake =  Faker(locale="ro_RO")
+Faker.seed(0)
+
+class Sex(Enum):
+    MALE = 1
+    FEMALE = 2
+    UNKNOWN = 3
+
+class Data:
+
+    def city(self, name: str)-> City:
+        return City.create(name=name)
+
+    def street(self, name: str)-> Street:
+        return Street.create(name=name)
+
+    def address(self, home_number: int, street: Street, city: City)-> Address:
+        return Address.create(
+            home_number=home_number,
+            street = street,
+            city = city)
+
+    def user(self, sex: Sex, address: Address)-> User:
+        match sex:
+            case Sex.MALE:
+                usr = User.create(
+                    firstname=fake.first_name_male(),
+                    secondname=fake.last_name_male(),
+                    sex = Sex.MALE.name,
+                    birthday = fake.date_of_birth(minimum_age=10, maximum_age=50),
+                    address = address)
+                return usr
+            case Sex.FEMALE:
+                usr = User.create(
+                    firstname=fake.first_name_female(),
+                    secondname=fake.last_name_female(),
+                    sex = Sex.FEMALE.name,
+                    birthday = fake.date_of_birth(minimum_age=10, maximum_age=50),
+                    address = address)
+                return usr
+            case _:
+                usr = User.create(
+                    firstname=fake.first_name_unisex(),
+                    secondname=fake.last_name_unisex(),
+                    sex = Sex.UNKNOWN.name,
+                    birthday = fake.date_of_birth(minimum_age=10, maximum_age=50),
+                    address = address)
+                return usr
