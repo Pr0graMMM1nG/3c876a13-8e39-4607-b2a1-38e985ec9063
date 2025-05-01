1131604e89a69f107e3dae9caac7b1b760d10381
alg0r1txm <alg0r1txm@protonmail.com>
2025-05-01 10:00:05 +0000

    create database using singleton pattern

---
+++ database.py
@@ -0,0 +1,20 @@
+import toml
+from peewee import SqliteDatabase
+
+
+class DataBaseMeta(type):
+    _instances: dict = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class DataBase(metaclass=DataBaseMeta):
+    @property
+    def sqlite(self):
+        with open('config.toml', mode='r') as file:
+            db_name = toml.load(file)['database']['name']
+            return SqliteDatabase(db_name)
