ec0357636186e472363e1022f4cd1ca93b827905
alg0r1txm <alg0r1txm@tuta.io>
2025-05-04 20:45:19 +0300

    Replace database filler with apartment generation

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from table import Table
+from models import City, Street
 
 
 def main():
     table = Table()
-    table.fill_database(100)
+    city = City.create(name="Chisinau")
+    street = Street.create(name="Alba Iulia")
+    for number in range(1, 10):
+        table.create_apartment(apartament_number=number, city=city, street=street)
+
 
 if __name__ == "__main__":
     main()
--- table.py
+++ table.py
@@ -1,5 +1,10 @@
+from tornado.autoreload import add_reload_hook
+
 from fake_data import Data, Sex
 from models import Address, City, Street, User, db
+from service import Apartment, Builder
+from random import Random
+from typing import Tuple, List
 
 
 class Table:
@@ -11,13 +16,33 @@ class Table:
         db.connect()
         db.create_tables([User, City, Street, Address])
 
-    def fill_database(self, count_row: int) -> None:
-        city = self._fake.city(name="Chisinau")
-        city.save()
-        street = self._fake.street(name="Alba Iulia")
-        street.save()
-        for count in range(0, count_row, 1):
-            address = self._fake.address(home_number=1,  city=city, street=street)
-            address.save()
-            user = self._fake.user(sex = Sex.MALE, address=address)
-            user.save()
+    def _create_adult(self, address: Address)-> Tuple[User, User]:
+        father = self._fake.user(sex= Sex.MALE, min_age=25, max_age=30, address = address)
+        mother = self._fake.user(sex=Sex.FEMALE, min_age=23, max_age=28, address = address)
+        return father, mother
+
+    def _create_child(self,address: Address)->List[User]:
+        child_list = []
+        random_child_count: int = Random().randint(a=0, b=5)
+        sex = [Sex.MALE, Sex.FEMALE]
+        random_child_sex: Sex = Random().choice(sex)
+        if random_child_count == 0:
+            return []
+        for _ in range(random_child_count):
+            child = self._fake.user(sex=random_child_sex, min_age=1, max_age=12, address = address)
+            child_list.append(child)
+        return child_list
+
+
+    def create_apartment(self, apartament_number: int, city: City, street: Street)-> None:
+        builder = Builder()
+        builder.set_city(city=city)
+        builder.set_street(street=street)
+        builder.set_address(home_number=apartament_number)
+        adult = self._create_adult(address=builder.address)
+        builder.set_adult(father=adult[0], mother=adult[1])
+        children = self._create_child(address=builder.address)
+        builder.set_child(children)
+
+
+
