b16452304fdd12d6bcd994a1f22132865443672c
alg0r1txm <alg0r1txm@tuta.io>
2025-05-04 20:44:38 +0300

    fix errors: add address to create users

--- fake_data.py
+++ fake_data.py
@@ -5,7 +5,7 @@ from faker import Faker
 from models import Address, City, Street, User
 
 fake =  Faker(locale="ro_RO")
-Faker.seed(0)
+# Faker.seed(0) uncomment if you want the data be the same every time
 
 class Sex(Enum):
     MALE = 1
@@ -26,29 +26,29 @@ class Data:
             street = street,
             city = city)
 
-    def user(self, sex: Sex, address: Address)-> User:
+    def user(self, sex: Sex, min_age: int, max_age: int, address: Address)-> User:
         match sex:
             case Sex.MALE:
                 usr = User.create(
                     firstname=fake.first_name_male(),
                     secondname=fake.last_name_male(),
                     sex = Sex.MALE.name,
-                    birthday = fake.date_of_birth(minimum_age=10, maximum_age=50),
-                    address = address)
+                    birthday = fake.date_of_birth(minimum_age=min_age, maximum_age=max_age),
+                    address= address)
                 return usr
             case Sex.FEMALE:
                 usr = User.create(
                     firstname=fake.first_name_female(),
                     secondname=fake.last_name_female(),
                     sex = Sex.FEMALE.name,
-                    birthday = fake.date_of_birth(minimum_age=10, maximum_age=50),
-                    address = address)
+                    birthday = fake.date_of_birth(minimum_age=min_age, maximum_age=max_age),
+                address = address)
                 return usr
             case _:
                 usr = User.create(
                     firstname=fake.first_name_unisex(),
                     secondname=fake.last_name_unisex(),
                     sex = Sex.UNKNOWN.name,
-                    birthday = fake.date_of_birth(minimum_age=10, maximum_age=50),
+                    birthday = fake.date_of_birth(minimum_age=min_age, maximum_age=max_age),
                     address = address)
                 return usr
